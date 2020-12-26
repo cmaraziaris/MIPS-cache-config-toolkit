739371f6628f71e2dadcd990a2f7dedcc25d8355
stelloop <sdi1900205@di.uoa.gr>
2020-12-26 22:56:53 +0200

    Program/Data cache size probably OK

---
+++ data_cache_size.s
@@ -0,0 +1,38 @@
+
+.org  0x10000000
+.data
+
+w: .word 1
+
+
+.org    0x80020000  
+.text
+.globl main
+
+main:
+
+ 
+  addi $t0, $zero, 0
+  addi $s0, $zero, 8192
+  la $s1, w
+
+Loop:
+
+  beq $t0, $s0, End
+  nop
+
+  lw $t5, 0($s1)
+
+  addi $s1, $s1, 4
+  addi $t0, $t0, 1
+   j Loop
+
+End:
+  addi $v0, $zero, 10
+  syscall
+
+
+
+
+
+
