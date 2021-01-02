8129a235c15b837205dd9005113585a3a14138d2
Harry <sdi1800105@di.uoa.gr>
2021-01-02 18:41:56 +0200

    cleaner data_block_size.s

--- block_size/data_block_size.s
+++ block_size/data_block_size.s
@@ -12,21 +12,19 @@ w: .word 1
 
 main:
 
-  addi $t0, $zero, 0
   addi $s0, $zero, 64
   la $s1, w
 
-Loop:
+loop:
 
-  beq $t0, $s0, End
-  nop
+  beq $s0, $zero, end
 
   lw $t5, 0($s1)
 
   addi $s1, $s1, 4
-  addi $t0, $t0, 1
-  j Loop
+  addi $s0, $s0, -1
+  j loop
 
-End:
+end:
   addi $v0, $zero, 10
   syscall
